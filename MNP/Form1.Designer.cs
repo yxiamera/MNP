e47b50a1d29af2fcfc851b10ef17f050d1feb441
Nikolayeva Yekaterina <yxiamera@users.noreply.github.com>
2020-09-15 22:08:08 +0300

    Add from flashdrive
    Done version of first lab

---
+++ MNP/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace MNP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.описаниеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ID_RED = new System.Windows.Forms.ToolStripMenuItem();
+            this.директивыРасчетаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ID_CALC = new System.Windows.Forms.ToolStripMenuItem();
+            this.видToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ID_INTERNET = new System.Windows.Forms.ToolStripMenuItem();
+            this.ID_EXIT = new System.Windows.Forms.ToolStripMenuItem();
+            this.ID_F = new System.Windows.Forms.ToolStripMenuItem();
+            this.ID_IO = new System.Windows.Forms.ToolStripMenuItem();
+            this.ID_PRIV = new System.Windows.Forms.ToolStripMenuItem();
+            this.ID_SYS = new System.Windows.Forms.ToolStripMenuItem();
+            this.ID_CONS = new System.Windows.Forms.ToolStripMenuItem();
+            this.ID_FILE = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.описаниеToolStripMenuItem,
+            this.ID_RED,
+            this.директивыРасчетаToolStripMenuItem,
+            this.ID_CALC,
+            this.видToolStripMenuItem,
+            this.ID_INTERNET,
+            this.ID_EXIT});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // описаниеToolStripMenuItem
+            // 
+            this.описаниеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ID_CONS,
+            this.ID_FILE});
+            this.описаниеToolStripMenuItem.Name = "описаниеToolStripMenuItem";
+            this.описаниеToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.описаниеToolStripMenuItem.Text = "Описание";
+            // 
+            // ID_RED
+            // 
+            this.ID_RED.Name = "ID_RED";
+            this.ID_RED.Size = new System.Drawing.Size(181, 22);
+            this.ID_RED.Text = "Редактирование";
+            // 
+            // директивыРасчетаToolStripMenuItem
+            // 
+            this.директивыРасчетаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ID_F,
+            this.ID_IO});
+            this.директивыРасчетаToolStripMenuItem.Name = "директивыРасчетаToolStripMenuItem";
+            this.директивыРасчетаToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.директивыРасчетаToolStripMenuItem.Text = "Директивы расчета";
+            // 
+            // ID_CALC
+            // 
+            this.ID_CALC.Name = "ID_CALC";
+            this.ID_CALC.Size = new System.Drawing.Size(181, 22);
+            this.ID_CALC.Text = "Расчет";
+            // 
+            // видToolStripMenuItem
+            // 
+            this.видToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ID_PRIV,
+            this.ID_SYS});
+            this.видToolStripMenuItem.Name = "видToolStripMenuItem";
+            this.видToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.видToolStripMenuItem.Text = "Вид";
+            // 
+            // ID_INTERNET
+            // 
+            this.ID_INTERNET.Name = "ID_INTERNET";
+            this.ID_INTERNET.Size = new System.Drawing.Size(181, 22);
+            this.ID_INTERNET.Text = "Интернет";
+            // 
+            // ID_EXIT
+            // 
+            this.ID_EXIT.Name = "ID_EXIT";
+            this.ID_EXIT.Size = new System.Drawing.Size(181, 22);
+            this.ID_EXIT.Text = "Выход";
+            this.ID_EXIT.Click += new System.EventHandler(this.ID_EXIT_Click);
+            // 
+            // ID_F
+            // 
+            this.ID_F.Name = "ID_F";
+            this.ID_F.Size = new System.Drawing.Size(243, 22);
+            this.ID_F.Text = "Вид частотной характеристики";
+            // 
+            // ID_IO
+            // 
+            this.ID_IO.Name = "ID_IO";
+            this.ID_IO.Size = new System.Drawing.Size(243, 22);
+            this.ID_IO.Text = "Вх/вых узлы";
+            // 
+            // ID_PRIV
+            // 
+            this.ID_PRIV.Name = "ID_PRIV";
+            this.ID_PRIV.Size = new System.Drawing.Size(196, 22);
+            this.ID_PRIV.Text = "Собственный браузер";
+            // 
+            // ID_SYS
+            // 
+            this.ID_SYS.Name = "ID_SYS";
+            this.ID_SYS.Size = new System.Drawing.Size(196, 22);
+            this.ID_SYS.Text = "Системный браузер";
+            // 
+            // ID_CONS
+            // 
+            this.ID_CONS.Name = "ID_CONS";
+            this.ID_CONS.Size = new System.Drawing.Size(177, 22);
+            this.ID_CONS.Text = "Ввод с клавиатуры";
+            // 
+            // ID_FILE
+            // 
+            this.ID_FILE.Name = "ID_FILE";
+            this.ID_FILE.Size = new System.Drawing.Size(177, 22);
+            this.ID_FILE.Text = "Ввод из файла";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem описаниеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ID_RED;
+        private System.Windows.Forms.ToolStripMenuItem директивыРасчетаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ID_F;
+        private System.Windows.Forms.ToolStripMenuItem ID_IO;
+        private System.Windows.Forms.ToolStripMenuItem ID_CALC;
+        private System.Windows.Forms.ToolStripMenuItem видToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ID_PRIV;
+        private System.Windows.Forms.ToolStripMenuItem ID_SYS;
+        private System.Windows.Forms.ToolStripMenuItem ID_INTERNET;
+        private System.Windows.Forms.ToolStripMenuItem ID_EXIT;
+        private System.Windows.Forms.ToolStripMenuItem ID_CONS;
+        private System.Windows.Forms.ToolStripMenuItem ID_FILE;
+    }
+}
+
