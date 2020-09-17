89eb77c35b49f97eac7fd4a562d48adc1013a26c
Nikolayeva Yekaterina <yxiamera@users.noreply.github.com>
2020-09-17 18:04:31 +0300

    100% of lab2 is done
    Added anti-fool protection

--- MNP/R.cs
+++ MNP/R.cs
@@ -21,10 +21,57 @@ namespace MNP
         {
             int i = Int32.Parse(m_nextResistor.Text);
 
-            //сделать защиту от дурака
-            GV.in_r[i, 0] = Int32.Parse(m_nPlusResistor.Text);
-            GV.in_r[i, 1] = Int32.Parse(m_nMinusResistor.Text);
-            GV.z_r[i] = Single.Parse(m_zResistor.Text);
+            //n+
+            try
+            {
+                GV.in_r[i, 0] = Int32.Parse(m_nPlusResistor.Text);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Ошибка: " + exception.Message + "\nЗначение n+ будет выставлено по умолчанию.", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                GV.in_r[i, 0] = 0;
+            }
+
+            if (GV.in_r[i, 0] < 0)
+            {
+                MessageBox.Show("Ошибка: Введенное число меньше нуля" + "\nЗначение n+ будет выставлено по умолчанию.", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                GV.in_r[i, 0] = 0;
+            }
+
+            //n-
+            try
+            {
+                GV.in_r[i, 1] = Int32.Parse(m_nMinusResistor.Text);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Ошибка: " + exception.Message + "\nЗначение n- будет выставлено по умолчанию.", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                GV.in_r[i, 1] = 0;
+            }
+
+            if (GV.in_r[i, 1] < 0)
+            {
+                MessageBox.Show("Ошибка: Введенное число меньше нуля" + "\nЗначение n- будет выставлено по умолчанию.", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                GV.in_r[i, 1] = 0;
+            }
+
+            //номинал
+            try
+            {
+                GV.z_r[i] = Single.Parse(m_zResistor.Text);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Ошибка: " + exception.Message + "\nЗначение номинала резистора будет выставлено по умолчанию.", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                GV.z_r[i] = 0;
+            }
+
+            if (GV.z_r[i] < 0)
+            {
+                MessageBox.Show("Ошибка: Введенное число меньше нуля" + "\nЗначение номинала резистора будет выставлено по умолчанию.", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                GV.z_r[i] = 0;
+            }
+
             i++;
             m_nextResistor.Text = i.ToString();
             if (i <= GV.resistorsValue)
